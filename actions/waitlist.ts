3e7d352fdb15d0835c85e2e2bf7f0b54c2b1a2f9
codify4 <kushta.joni@gmail.com>
2025-07-21 15:56:52 +0200

    feat: production backend url

--- actions/waitlist.ts
+++ actions/waitlist.ts
@@ -4,7 +4,7 @@ export async function addToWaitlist(formData: FormData) {
     const name = formData.get('name') as string
     const email = formData.get('email') as string
 
-    const response = await fetch(`${process.env.DEV_BACKEND_URL}/waitlist`, {
+    const response = await fetch(`${process.env.DEV_BACKEND_URL || process.env.PROD_BACKEND_URL}/waitlist`, {
         method: 'POST',
         body: JSON.stringify({ name, email }),
         headers: {
